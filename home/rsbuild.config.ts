155a6bfbe0ef5069f3333cb1a41b33b9249538f9
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-06-19 01:59:23 -0300

    add config

--- home/rsbuild.config.ts
+++ home/rsbuild.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
-import {
-	NativeFederationTestsHost,
-	NativeFederationTestsRemote,
-} from "@module-federation/native-federation-tests/rspack";
 
 const PORT: number = 3000;
-
-const moduleFederationConfig = {
-  name: 'home',
-  exposes: {
-    './button': './src/components/Button/index.tsx'
-  },
-  shared: ['react', 'react-dom'],
-}
 
 export default defineConfig({
   server: {
@@ -27,9 +15,13 @@ export default defineConfig({
     rspack: ( config, { appendPlugins  }) => {
       config.output!.uniqueName = 'home';
       appendPlugins ([
-        new ModuleFederationPlugin(moduleFederationConfig),
-        NativeFederationTestsHost({moduleFederationConfig}),
-        NativeFederationTestsRemote({moduleFederationConfig})
+        new ModuleFederationPlugin({
+          name: 'home',
+          exposes: {
+            './button': './src/components/Button/index.tsx'
+          },
+          shared: ['react', 'react-dom'],
+        }),
       ]);
     },
   },
