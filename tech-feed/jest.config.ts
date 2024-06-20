7c1768af3956fb0c0f40a94c5b17d0977db72584
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-06-20 02:19:56 -0300

    test: add ajustsments in test tech-feed config

--- tech-feed/jest.config.ts
+++ tech-feed/jest.config.ts
@@ -1,11 +1,17 @@
 export default {
 	testEnvironment: "jest-environment-jsdom",
 	setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+	coverageDirectory : 'coverage',
 	collectCoverageFrom: [
 		'**/*.{ts,tsx,js,jsx}',
 		'!**/node_modules/**',
 		'!**/vendor/**',
-	  ],
+		'!<rootDir>/tests/**/*.{ts,tsx,js,jsx}',
+		'!<rootDir>/coverage/**/*.{ts,tsx,js,jsx}',
+		'!<rootDir>/src/bootstrap.{ts,tsx,js,jsx}',
+		'!<rootDir>/src/index.{ts,tsx,js,jsx}',
+		'!<rootDir>/rsbuild.config.{ts,js}'
+	],
 	transform: {
 		"^.+\\.(t|j)sx?$": [
 			"@swc/jest",
@@ -27,7 +33,7 @@ export default {
 	},
 	moduleNameMapper: {
 		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-			"<rootDir>/tests/__mocks__/fileMock.js",
-		"\\.(css|less|scss|sass)$": "<rootDir>/tests/__mocks__/styleMock.js",
+			"<rootDir>/tests/mocks/file-mock.js",
+		"\\.(css|less|scss|sass)$": "<rootDir>/tests/mocks/style-mock.js",
 	},
 };
