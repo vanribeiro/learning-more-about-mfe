8f931e8d4aaf97534f25a59359371f80f023c636
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-06-20 02:18:49 -0300

    test: add jest and react testing library in home mfe

---
+++ home/jest.config.ts
@@ -0,0 +1,39 @@
+export default {
+	testEnvironment: "jest-environment-jsdom",
+	setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+	coverageDirectory : 'coverage',
+	collectCoverageFrom: [
+		'**/*.{ts,tsx,js,jsx}',
+		'!**/node_modules/**',
+		'!**/vendor/**',
+		'!<rootDir>/tests/**/*.{ts,tsx,js,jsx}',
+		'!<rootDir>/coverage/**/*.{ts,tsx,js,jsx}',
+		'!<rootDir>/src/bootstrap.{ts,tsx,js,jsx}',
+		'!<rootDir>/src/index.{ts,tsx,js,jsx}',
+		'!<rootDir>/rsbuild.config.{ts,js}'
+	],
+	transform: {
+		"^.+\\.(t|j)sx?$": [
+			"@swc/jest",
+			{
+				jsc: {
+					parser: {
+						tsx: true,
+						syntax: "typescript",
+					},
+					transform: {
+						react: {
+							runtime: "automatic",
+						},
+					},
+				},
+				isModule: "unknown",
+			},
+		],
+	},
+	moduleNameMapper: {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+			"<rootDir>/tests/mocks/file-mock.js",
+		"\\.(css|less|scss|sass)$": "<rootDir>/tests/mocks/style-mock.js",
+	},
+};
