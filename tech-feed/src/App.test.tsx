f01a49cc492f35dc3f72d338ac539613f1715ee4
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-06-20 20:22:49 -0300

    test: add a lib to mock fetch api request

--- tech-feed/src/App.test.tsx
+++ tech-feed/src/App.test.tsx
@@ -1,10 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { server } from './../tests/services/node'
+
+beforeAll(() => {
+    return server.listen();
+});
+
+afterEach(() => {
+    return server.resetHandlers();
+});
+
+afterAll(() => {
+    return server.close();
+})
 
 describe('App component', () => {
     it('should render the consumer component', () => {
         render(<App />);
         const feed = screen.getByText('Consumer');
+        // @ts-ignore
         expect(feed).toBeInTheDocument();
     });
 });
